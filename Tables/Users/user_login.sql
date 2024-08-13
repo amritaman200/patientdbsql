1e89873ebe9fd27406afab57ed97978183d49e5e
p.amrit <p.amrit@collaberadigital.com>
2024-08-13 17:37:11 +0530

    user login and user profile

---
+++ Tables/Users/user_login.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE user_login (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    ic_number VARCHAR(20)  NULL,
+    passportnumber VARCHAR(20)  NULL,
+    mobilenumber VARCHAR(15)  NULL,
+    emailid VARCHAR(50) NULL,
+    otp VARCHAR(10) NOT NULL,
+	CreatedBy nvarchar(50) NOT NULL,
+	CreatedDate datetime,
+	UpdatedBy nvarchar(50),
+	UpdatedDate datetime,
+	FacilityCode nvarchar(100)
+);
---
+++ Tables/Users/user_profile.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE user_profile (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    nickname VARCHAR(50),
+    date_of_birth DATE NOT NULL,
+    gender CHAR(1) CHECK (gender IN ('M', 'F', 'O')) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_login(user_id),
+	CreatedBy nvarchar(50) NOT NULL,
+	CreatedDate datetime,
+	UpdatedBy nvarchar(50),
+	UpdatedDate datetime
+);
